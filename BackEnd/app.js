da39fc881af20eb68996b3ae420667d9bb05eb77
DeboDevelop <debajyotidofficial@gmail.com>
2021-01-14 13:59:15 +0530

    Added GET method to query for student, batch and the students who are enrolled in a batch. Also added POST request for a student to join a batch.

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -50,6 +50,33 @@ router.get("/batch", (req, res) => {
     });
 });
 
+router.get("/studentbatch", (req, res) => {
+    let sql =
+        "SELECT Student.FirstName, Student.LastName, Batch.BatchName FROM StudentBatchRelation INNER JOIN Student ON Student.ID = StudentBatchRelation.StudentID INNER JOIN Batch ON Batch.ID = StudentBatchRelation.BatchID";
+    let query = db.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err });
+        } else if (result) {
+            res.status(200).json(result);
+        } else {
+            res.status(400).json({ message: "Bad requiest" });
+        }
+    });
+});
+
+router.get("/batch", (req, res) => {
+    let sql = "SELECT * FROM Batch";
+    let query = db.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err });
+        } else if (result) {
+            res.status(200).json(result);
+        } else {
+            res.status(400).json({ message: "Bad requiest" });
+        }
+    });
+});
+
 router.post("/student", (req, res) => {
     if (
         req.body.lastname === null ||
@@ -57,21 +84,40 @@ router.post("/student", (req, res) => {
         req.body.firstname === null ||
         req.body.firstname === ""
     ) {
-        res.status(400).json({ message: "Bad requiest" });
+        res.status(400).json({ message: "Bad request" });
     }
     let sql =
         "INSERT INTO `Student` (`ID`, `LastName`, `FirstName`) VALUES (NULL," +
-        "'" +
-        req.body.lastname +
-        "'," +
-        "'" +
-        req.body.firstname +
-        "')";
+        mysql.escape(req.body.lastname) +
+        "," +
+        mysql.escape(req.body.firstname) +
+        ")";
     let query = db.query(sql, (err, result) => {
         if (err) {
             res.status(500).json({ error: err });
         } else if (result) {
             res.status(200).json({ message: "Student Added" });
+        } else {
+            res.status(400).json({ message: "Bad requiest" });
+        }
+    });
+});
+
+router.post("/join", (req, res) => {
+    if (req.query.batch === null || req.query.batch === "" || req.query.student === null || req.query.student === "") {
+        res.status(400).json({ message: "Bad request" });
+    }
+    let sql =
+        "INSERT INTO `StudentBatchRelation` (`ID`, `StudentID`, `BatchID`) VALUES (NULL," +
+        mysql.escape(req.query.student) +
+        "," +
+        mysql.escape(req.query.batch) +
+        ")";
+    let query = db.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err });
+        } else if (result) {
+            res.status(200).json({ message: "Student Added to Batch" });
         } else {
             res.status(400).json({ message: "Bad requiest" });
         }
